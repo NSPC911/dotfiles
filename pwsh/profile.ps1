698db984e622f0e8ae58af256ef8a9438ebf5cbb
NSPC911 <87571998+NSPC911@users.noreply.github.com>
2025-07-02 16:13:00 +0800

    July 2 dump

--- pwsh/profile.ps1
+++ pwsh/profile.ps1
@@ -6,6 +6,8 @@ uv generate-shell-completion powershell | Out-String | Invoke-Expression
 regolith completion powershell | Out-String | Invoke-Expression
 zoxide init powershell --cmd zd | Out-String | Invoke-Expression
 onefetch --generate powershell | Out-String | Invoke-Expression
+gh completion -s powershell | Out-String | Invoke-Expression
+gh copilot alias -- pwsh | Out-String | Invoke-Expression
 
 ##### cool zoxide + onefetch #####
 # waste of time
@@ -19,101 +21,6 @@ onefetch --generate powershell | Out-String | Invoke-Expression
 # 		onefetch --no-color-palette --nerd-fonts
 # 	}
 # }
-
-##### Copilot really is built different bruh ####
-function ghcs {
-	# Debug support provided by common PowerShell function parameters, which is natively aliased as -d or -db
-	# https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_commonparameters?view=powershell-7.4#-debug
-	param(
-		[Parameter()]
-		[string]$Hostname,
-
-		[ValidateSet('gh', 'git', 'shell')]
-		[Alias('t')]
-		[String]$Target = 'shell',
-
-		[Parameter(Position=0, ValueFromRemainingArguments)]
-		[string]$Prompt
-	)
-	begin {
-		# Create temporary file to store potential command user wants to execute when exiting
-		$executeCommandFile = New-TemporaryFile
-
-		# Store original value of GH_* environment variable
-		$envGhDebug = $Env:GH_DEBUG
-		$envGhHost = $Env:GH_HOST
-	}
-	process {
-		if ($PSBoundParameters['Debug']) {
-			$Env:GH_DEBUG = 'api'
-		}
-
-		$Env:GH_HOST = $Hostname
-
-		gh copilot suggest -t $Target -s "$executeCommandFile" $Prompt
-	}
-	end {
-		# Execute command contained within temporary file if it is not empty
-		if ($executeCommandFile.Length -gt 0) {
-			# Extract command to execute from temporary file
-			$executeCommand = (Get-Content -Path $executeCommandFile -Raw).Trim()
-
-			# Insert command into PowerShell up/down arrow key history
-			[Microsoft.PowerShell.PSConsoleReadLine]::AddToHistory($executeCommand)
-
-			# Insert command into PowerShell history
-			$now = Get-Date
-			$executeCommandHistoryItem = [PSCustomObject]@{
-				CommandLine = $executeCommand
-				ExecutionStatus = [Management.Automation.Runspaces.PipelineState]::NotStarted
-				StartExecutionTime = $now
-				EndExecutionTime = $now.AddSeconds(1)
-			}
-			Add-History -InputObject $executeCommandHistoryItem
-
-			# Execute command
-			Write-Host "`n"
-			Invoke-Expression $executeCommand
-		}
-
-		# Clean up temporary file used to store potential command user wants to execute when exiting
-		Remove-Item -Path $executeCommandFile
-
-		# Restore GH_* environment variables to their original value
-		$Env:GH_DEBUG = $envGhDebug
-	}
-}
-
-function ghce {
-	# Debug support provided by common PowerShell function parameters, which is natively aliased as -d or -db
-	# https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_commonparameters?view=powershell-7.4#-debug
-	param(
-		[Parameter()]
-		[string]$Hostname,
-
-		[Parameter(Position=0, ValueFromRemainingArguments)]
-		[string[]]$Prompt
-	)
-	begin {
-		# Store original value of GH_* environment variables
-		$envGhDebug = $Env:GH_DEBUG
-		$envGhHost = $Env:GH_HOST
-	}
-	process {
-		if ($PSBoundParameters['Debug']) {
-			$Env:GH_DEBUG = 'api'
-		}
-
-		$Env:GH_HOST = $Hostname
-
-		gh copilot explain $Prompt
-	}
-	end {
-		# Restore GH_* environment variables to their original value
-		$Env:GH_DEBUG = $envGhDebug
-		$Env:GH_HOST = $envGhHost
-	}
-}
 
 ##### Superfile Go To Last Dir #####
 function spf {
